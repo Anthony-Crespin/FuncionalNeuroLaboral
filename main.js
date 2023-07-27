b7b3ed050bf2cd9d070035ec1a463168b7e2a4e0
Anthony-Crespin <dcris3344@gmail.com>
2023-07-26 19:27:40 -0500

    Subiendo pagina de fnl

---
+++ main.js
@@ -0,0 +1,17 @@
+
+window.addEventListener('scroll', function(ev){
+    let nav = document.querySelector("nav");
+
+    nav.classList.toggle("pined", window.scrollY > 0);
+});
+
+function MostrarMenu(){
+    let btnBurger = document.getElementById("icon");
+    let menu = document.querySelector(".menu");
+
+    btnBurger.addEventListener('click', function(){
+        menu.classList.toggle("show");
+    })
+}
+
+MostrarMenu();
